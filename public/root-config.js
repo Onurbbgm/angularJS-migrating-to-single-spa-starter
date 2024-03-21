036b1ee613511648defff4429db8926c37f4c5f5
Bruno Moretto <brunogmoretto@hotmail.com>
2024-03-21 16:08:11 -0300

    code using observers

--- public/root-config.js
+++ public/root-config.js
@@ -1,17 +1,28 @@
 let sharedScpope = []; //If there is  a scope that needs to be shared between the apps so is not initialized multiple times.
 System.import("single-spa").then((singleSpa) => {
-    const { registerApplication, start } = singleSpa;
-    registerApplication(
-        "rootComponent",
-        () => System.import('reactMigration').then((reactMigration) => {
-            console.log(reactMigration);
-            reactMigration.init(sharedScpope);
-            return reactMigration.get('RootComponent').then(module => {
-                return module();
-            });
-        }),
-        location => location.pathname.startsWith('/')
-    );
+    const { registerApplication, start, getAppStatus, NOT_REGISTERED } = singleSpa;
+    console.log('app status', getAppStatus('rootComponent'));
+    const observer = new MutationObserver((mutations, obs) => {
+        if (document.querySelector('#reactRootComponent') && getAppStatus('rootComponent') == null) {
+            registerApplication(
+                "rootComponent",
+                () => System.import('reactMigration').then((reactMigration) => {
+                    console.log(reactMigration);
+                    reactMigration.init(sharedScpope);
+                    return reactMigration.get('RootComponent').then(module => {
+                        return module();
+                    });
+                }),
+                location => location.pathname.startsWith('/')
+            );
+            obs.disconnect();
+        }
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
 
     registerApplication(
         "someOtherComponent",
